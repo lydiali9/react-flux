4cf3cd105a77e772b109476038fbaa011098285c
Lynn Li <648823000@qq.com>
2018-01-09 18:46:33 -0800

    [feaure]: add logo for header

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -32125,7 +32125,7 @@ var Header = React.createClass({displayName: "Header",
         React.createElement("nav", {className: "navbar navbar-default"}, 
           React.createElement("div", {className: "container-fluid"}, 
               React.createElement("a", {href: "/", className: "navbar-brand"}, 
-                React.createElement("img", {src: "images/logo.png"})
+                React.createElement("img", {width: "50", src: "images/logo.png"})
               ), 
               React.createElement("ul", {className: "nav navbar-nav"}, 
                 React.createElement("li", null, React.createElement("a", {href: "/"}, "Home")), 
