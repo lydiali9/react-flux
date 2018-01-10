bf779ebafc4a6b4c50f536cecd56115543f77bca
Lynn Li <648823000@qq.com>
2018-01-09 18:21:57 -0800

    [feature]: add some third plu

--- src/main.js
+++ src/main.js
@@ -1,31 +1,33 @@
-"use strict";
-
 $ = jQuery = require('jquery');
 var React = require('react');
 var Home = require('./components/homePage');
 var About = require('./components/about/aboutPage');
 
-var App = React.createClass({
-	render: function() {
-		var Child;
+(function(win) {
+	"use strict";
+	
+	var App = React.createClass({
+		render: function() {
+			var Child;
 
-		switch(this.props.route) {
-			case 'about': Child = About; break;
-			default: Child = Home;
+			switch(this.props.route) {
+				case 'about': Child = About; break;
+				default: Child = Home;
+			}
+
+			return (
+				<div>
+					<Child/>
+				</div>
+			);
 		}
+	});
 
-		return (
-			<div>
-				<Child/>
-			</div>
-		);
+	function render() {
+		var route = window.location.hash.substr(1);
+		React.render(<App route={route} />, document.getElementById('app'));
 	}
-});
 
-function render() {
-	var route = window.location.hash.substr(1);
-	React.render(<App route={route} />, document.getElementById('app'));
-}
-
-window.addEventListener('hashchange', render);
-render();
+	win.addEventListener('hashchange', render);
+	render();
+})(window);
