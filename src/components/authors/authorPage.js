a4890293587c0afeca3a46181476e9bc76dd688d
Lynn Li <648823000@qq.com>
2018-01-10 20:51:16 -0800

    [feature]: add new page

--- src/components/authors/authorPage.js
+++ src/components/authors/authorPage.js
@@ -1,8 +1,10 @@
 "use strict";
 
 var React = require('react');
+var Router = require('react-router');
 var AuthorAPi = require('../../api/authorApi');
 var AuthorList = require('./authorList');
+var Link = Router.Link;
 
 var AuthorPage = React.createClass({
 	getInitialState: function() {
@@ -18,18 +20,10 @@ var AuthorPage = React.createClass({
 	},
 
 	render: function() {
-		var createAuthorRow = function(author) {
-			return (
-				<tr key={author.id}>
-					<td><a href={"/#authors/" + author.id}>{author.id}</a></td>
-					<td>{author.firstName} {author.lastName}</td>
-				</tr>
-			);
-		};
-
 		return (
 			<div>
 				<h1>Authors</h1>
+				<Link to="addAuthor" className="btn btn-default">Add Author</Link>
 				<AuthorList authors={this.state.authors} />
 			</div>
 		);
