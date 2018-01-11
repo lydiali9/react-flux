cf66fe5307cc1900f6ec1f7fa1218b45d305144a
Lynn Li <648823000@qq.com>
2018-01-10 21:44:18 -0800

    [feature]: add function to add authors

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -1,9 +1,14 @@
 "use strict";
 
 var React = require('react');
+var Router = require('react-router');
 var AuthorForm = require('./authorForm');
+var AuthorAPi = require('../../api/authorApi');
 
 var ManageAuthorPage = React.createClass({
+	mixins: [
+		Router.Navigation
+	],
 	getInitialState: function() {
 		return {
 			author: { id: '', firstName: '', lastName: '' }
@@ -15,10 +20,17 @@ var ManageAuthorPage = React.createClass({
 		this.state.author[field] = value;
 		return this.setState({author: this.state.author});
 	},
+	saveAuthor: function(event) {
+		event.preventDefault();
+		AuthorAPi.saveAuthor(this.state.author);
+		this.transitionTo('authors');
+	},
 	render: function() {
 		return (
-			<AuthorForm author={this.state.author} onChange={this.setAuthorState}
-			/>
+			<AuthorForm 
+				author={this.state.author} 
+				onChange={this.setAuthorState}
+				onSave={this.saveAuthor} />
 		);
 	}
 });
